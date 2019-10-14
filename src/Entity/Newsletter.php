5061010f43404756ecafedc77e42c1938f8b288c
Carine <you@example.com>
2019-10-14 10:18:08 +0200

    création contact + trait factorisation id

--- src/Entity/Newsletter.php
+++ src/Entity/Newsletter.php
@@ -2,20 +2,15 @@
 
 namespace App\Entity;
 
-use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\ORM\Mapping as ORM;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\NewsletterRepository")
  */
 class Newsletter
 {
-    /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+    use BaseTableTrait;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -27,21 +22,10 @@ class Newsletter
      */
     private $subscribed;
 
-    /**
-     * @Gedmo\Timestampable(on="create")
-     * @ORM\Column(type="datetime")
-     */
-    private $created;
 
     public function __construct()
     {
     $this->subscribed = true;
-    }
-
-    
-    public function getId(): ?int
-    {
-        return $this->id;
     }
 
     public function getEmail(): ?string
@@ -68,15 +52,4 @@ class Newsletter
         return $this;
     }
 
-    public function getCreated(): ?\DateTimeInterface
-    {
-        return $this->created;
-    }
-
-    public function setCreated(\DateTimeInterface $created): self
-    {
-        $this->created = $created;
-
-        return $this;
-    }
 }
